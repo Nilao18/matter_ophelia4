14d806733bdcd35ccb40f97f99cb52d196551864
Nilao18 <128127941+Nilao18@users.noreply.github.com>
2026-02-22 20:24:46 +0100

    Create humidity_sensor.h

---
+++ src/humidity_sensor.h
@@ -0,0 +1,12 @@
+/*
+ * Humidity Sensor - Dynamic Endpoint for Matter
+ * Device Type: Humidity Sensor (0x0307)
+ * Cluster: Relative Humidity Measurement (0x0405)
+ */
+
+#pragma once
+
+#include <lib/core/CHIPError.h>
+
+CHIP_ERROR InitHumiditySensorEndpoint();
+void SetMeasuredHumidity(uint16_t value);
