4766b0e9f6f948158df552502c82a03fccccbd32
Nilao18 <128127941+Nilao18@users.noreply.github.com>
2026-02-23 11:50:27 +0100

    Update humidity_sensor.h

--- src/humidity_sensor.h
+++ src/humidity_sensor.h
@@ -1,6 +1,6 @@
 /*
  * Humidity Sensor - Dynamic Endpoint for Matter
- * Device Type: Humidity Sensor (0x0307)
+ * Device Type: Humidity Sensor (0x0015)
  * Cluster: Relative Humidity Measurement (0x0405)
  */
 
